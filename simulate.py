d4c8bf1c1b7ae43c96b7fc0e7cbc0b3871de05a9
ismail.ouazzani <ismail.ouazzani@mail.utoronto.ca>
2025-03-22 18:43:52 -0400

    refactoring

--- simulate.py
+++ simulate.py
@@ -7,7 +7,8 @@ Cost: USD
 """
 from data import import_merra2_dataset, DATASETS
 from geography import mask_lon_lat, plot_heatmap
-from pvsystem import make_pv_system, appraise_system
+from pv_system import make_pv_system, appraise_system
+from water_system import calculate_volume_water_pumped, calculate_volume_water_demand, simulate_water_tank, plot_water_simulation
 import pvlib
 import pandas as pd
 from tqdm import tqdm
@@ -38,49 +39,6 @@ STORAGE_FACTOR = 0.65  # optimal result in the paper
 NUMBER_SOLAR_PANELS = 43  # optimal result in Bouzidi paper
 HYDRAULIC_CONSTANT = 2.725
 
-def calculate_volume_water_pumped(num_panels, power, time_range, head, pump_eff, inverter_eff, hydraulic_const):
-    # TODO: how many solar panels can I fit per inverter?
-    return (num_panels * power * time_range * pump_eff * inverter_eff) / (hydraulic_const * head) # TODO: am I allowed to just multiply with num solar panels?
-
-
-def calculate_volume_water_demand(hourly_need, time_range):
-    return hourly_need * time_range
-
-def plot_water_simulation(results, time_range, tank_capacity, title):
-    """
-    Plot water simulation for a specified time range.
-    """
-    results.loc[time_range, "water_pumped"].plot()
-    results.loc[time_range, "water_demand"].plot()
-    results.loc[time_range, "water_in_tank"].plot()
-
-    plt.axhline(y=tank_capacity, color='r', linestyle='--', label="Tank capacity")
-    plt.legend(["Water pumped", "Water Demand", "Water in tank", "Tank capacity"])
-    plt.title(title)
-    plt.ylabel("Water (m^3)")
-    plt.savefig("outputs/water_in_tank.png")
-
-def simulate_water_tank(results: pd.DataFrame, tank_capacity: float) -> pd.DataFrame:
-    """
-    Given a DataFrame 'results' containing water pumped and water demand columns,
-    compute the water level in the tank and the water deficit over time.
-    """
-    water_in_tank: list[float] = [tank_capacity]  # initial condition: full tank
-    water_deficit: list[float] = [0]
-
-    for i in range(1, len(results)):
-        new_level: float = water_in_tank[-1] + results["water_pumped"].iloc[i] - results["water_demand"].iloc[i]
-        if new_level < 0:
-            water_deficit.append(abs(new_level))
-            constrained_level: float = 0
-        else:
-            constrained_level = min(new_level, tank_capacity)
-            water_deficit.append(0)
-        water_in_tank.append(constrained_level)
-
-    results["water_in_tank"] = water_in_tank
-    results["water_deficit"] = water_deficit
-    return results
 
 def simulate_at_location(
     longitude: float,
